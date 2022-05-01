62292b965f42feb89ba269a734d87e69ca960b53
gnpaone <gnpaone@users.noreply.github.com>
2022-05-01 08:21:44 +0000

    deploy: gnpaone/Naveen-Personal-Website@253079b8bac8365728fc6976e3ac4fdaae3b289b

---
+++ precache-manifest.62d2442a43099406fcae2501bcb5ad55.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51c1b09434530db7b7c4cfc6b22cb2b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b62832cd119f2265ae71",
+    "url": "/static/css/main.223b60ac.chunk.css"
+  },
+  {
+    "revision": "6b3d187d0af3fcd0973d",
+    "url": "/static/js/2.f058e336.chunk.js"
+  },
+  {
+    "revision": "764939f535c288e48ccb65554828fbe4",
+    "url": "/static/js/2.f058e336.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b62832cd119f2265ae71",
+    "url": "/static/js/main.b59e1bae.chunk.js"
+  },
+  {
+    "revision": "08a30d57f53c2273c63e",
+    "url": "/static/js/runtime-main.6c670bdb.js"
+  },
+  {
+    "revision": "79511c64c69c95c90637914bd9e973dd",
+    "url": "/static/media/about.79511c64.md"
+  },
+  {
+    "revision": "2f73a0983171b80ce033b915210d44ac",
+    "url": "/static/media/download.2f73a098.png"
+  },
+  {
+    "revision": "1f0ac8fa956527f80d52eac8f60c141d",
+    "url": "/static/media/food_1.1f0ac8fa.jpg"
+  },
+  {
+    "revision": "d3a19dbcf8c415a3e1c02ae34db28dc0",
+    "url": "/static/media/food_2.d3a19dbc.jpg"
+  },
+  {
+    "revision": "240f733d36894924ebae2128af3adf5e",
+    "url": "/static/media/lunar-landing.240f733d.gif"
+  },
+  {
+    "revision": "ad1f17884dd45a46768335b454474246",
+    "url": "/static/media/lunar-landing.ad1f1788.jpg"
+  },
+  {
+    "revision": "6c47d1b74bf8e42aeb10ad95dc0ae125",
+    "url": "/static/media/mac_1.6c47d1b7.png"
+  },
+  {
+    "revision": "4ab937d512cea83ed04c43232c12defc",
+    "url": "/static/media/open-ended.4ab937d5.mp3"
+  },
+  {
+    "revision": "be37616bacd8c0218ac40dac406013e4",
+    "url": "/static/media/website_1.be37616b.png"
+  },
+  {
+    "revision": "15757e5c76f26cc872a8bd4f37dbcfdf",
+    "url": "/static/media/website_2.15757e5c.png"
+  },
+  {
+    "revision": "78914ba9c19ef554aca22df9d1249652",
+    "url": "/static/media/website_3.78914ba9.png"
+  },
+  {
+    "revision": "bf6e293515220c664f3d980c2c9578b1",
+    "url": "/static/media/winget_1.bf6e2935.jpg"
+  },
+  {
+    "revision": "17877bd756ff6b3fe6bfca54e7732708",
+    "url": "/static/media/winget_2.17877bd7.jpg"
+  }
+]);
